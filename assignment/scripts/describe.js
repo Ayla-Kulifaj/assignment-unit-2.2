c99d6ea65071d8e0ef8589824496ee1f54d83022
Ayla Kulifaj <aylakulifaj@gmail.com>
2024-03-03 11:08:55 -0600

    assignment 2

--- assignment/scripts/describe.js
+++ assignment/scripts/describe.js
@@ -26,10 +26,14 @@ if (number >= 2) {
 
 
 // 1. WRITE YOUR DESCRIPTION HERE
-// 
-// 
-// 
+// Assigning a variable called name to the value Dane. 
+// Creating a conditional statement
+// The computer will first see if the name is Mary and will print a particular thing
+// If the computer sees the name is not Mary the computer will do another action (print a different thing). 
+// In this case, since name is not Mary, the computer will console.log 'how do you do?'
+// With if/else/else if statements the computer is checking for True or False and told to do a certain action regarding if it is either true or false
 //
+
 
 //CODE
 /*
@@ -45,10 +49,13 @@ if (name === 'Mary') {
 */
 
 // 2. WRITE YOUR DESCRIPTION HERE
-// 
-// 
-// 
-//
+// Creating a variable called secret 
+// Creating a variable called code and assigning it to the number 123
+// Starting a conditional statement and the computer first goes to see if the variable code is equal to the number 123. Which it is in this case.
+// If code is equal to 123, then we assign the variable secret to the string 'super' and sets the value of code equal to the value of code times 2
+// Starting another conditional statement that checks if code is greater than the value 250, and if it is, the computer will set the variabke equal to 'duper'.
+// The value of code would be 246 which is less than 250, so we would not go into this conditional.
+// Lastly, we console.log the value of secret which is 'super'.
 
 //CODE
 /*
@@ -70,10 +77,16 @@ console.log(secret)
 */
 
 // 3. WRITE YOUR DESCRIPTION HERE
-// 
-// 
-// 
-//
+// Create a boolean variable called isStudent, and set it equal to true
+// Create a variable called age and set it equal to the number 34
+// Create a variable called zip and set it equal to the number 55407
+// Starting a conditional statement that checks if the variable isStudent is equal to true AND the variable zip is greater than the number 80000. If this is true the computer would console.log `You're a student on the West Coast!`
+// isStudent is true but zip is less than 80000, so we move on to the next condition. 
+// Then the computer checks to see if isStudent is equal to false OR age is less than the number 30. If this is true, the computer would console.log 'What are your hobbies?'
+// isStudent is not equal to false and age is greater than 30, so the computer moves on to the next condition
+// The computer then checks if isStudent is equal to true, if it is it will console.log 'Welcome to Prime!'. 
+// This condition is met, so that is the output. 
+// If that condition was not met, it would default to console.log 'How about the weather?'
 
 //CODE
 /*
@@ -115,7 +128,7 @@ let number = 1;
 
 // FIX - number-- decrements number, but the instructions ask to increment. 
 // Should be number++.
-number--; 
+number--;
 
 if (number >= 2) {
   console.log('yes');
@@ -130,12 +143,18 @@ if (number >= 2) {
 // -- it is, so we set colorOne and colorTwo to 'purple'
 
 /*
+// FIX - colorOne is suppose to be set to 'blue'
+// should be let colorOne = 'blue'
 let colorOne = 'red';
+// FIX - colorTwo is suppse to be set to 'red'
+// should be let colorTwo = 'red'
 let colorTwo = 'blue';
 let mix = true;
 
 if (mix === true) {
   colorOne = 'purple';
+  // FIX - the problem wants both colorOne and colorTwo set to 'purple'
+  // add in colorTwo = 'purple';
 }
 */
 
@@ -145,8 +164,12 @@ if (mix === true) {
 
 /*
 let temp = 40;
+// FIX - the problem doesn't say to make time a constant variable
+// should be let time = 4;
 const time = 4;
 
+// FIX - the problem wants both conditions met but the conditional is using OR instead of AND 
+// should be if (temp > 39 && time >= 4) {
 if (temp > 39 || time >= 4) {
   console.log('throw away the food!');
 }
@@ -158,8 +181,18 @@ if (temp > 39 || time >= 4) {
 
 /*
 let age = 21;
+// FIX - the problem doesn't say to make minAge a constant variable
+// should be let minAge = 21;
 const minAge = 21;
 
+// FIX - the problem is asking to check if age is greater than or equal to minAge, but this
+// conditional is checking if minAge is less than or equal to age
+// should be 
+// if(age >= minAge) {
+//   console.log('enter');
+// } else {
+//   console.log('no entry');
+// }
 if(minAge <= age) {
   console.log('no entry');
 } else {
